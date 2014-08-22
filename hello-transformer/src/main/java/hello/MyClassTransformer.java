632a5d722cebed96dda7fd5a80a3168a27978318
Nicky Mølholm <nicky.moelholm@schneider-electric.com>
2014-08-22 22:49:56 +0200

    smaller cleanup round

--- hello-transformer/src/main/java/hello/MyClassTransformer.java
+++ hello-transformer/src/main/java/hello/MyClassTransformer.java
@@ -1,14 +1,21 @@
 package hello;
 
+import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.IllegalClassFormatException;
+import java.net.URL;
 import java.security.ProtectionDomain;
 
-public class MyClassTransformer implements java.lang.instrument.ClassFileTransformer {
+public class MyClassTransformer implements ClassFileTransformer {
 
     @Override
     public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined,
             ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
-        System.out.println("MyClassTransformer.transform() :: "+className+" ("+protectionDomain.getCodeSource().getLocation()+")");
+
+        // This is where you would transform the bytecode !
+
+        URL classLocation = protectionDomain.getCodeSource().getLocation();
+        System.out.format("MyClassTransformer.transform() :: %s (%s)%n", className, classLocation);
+
         return classfileBuffer;
     }
 
