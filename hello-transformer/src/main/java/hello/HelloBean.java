a976f80f8804e25cd318938c62b5845aae309073
Nicky Mølholm <nicky.moelholm@schneider-electric.com>
2014-08-23 11:43:39 +0200

    Changed project from EJB-JAR to WAR

--- hello-transformer/src/main/java/hello/HelloBean.java
+++ hello-transformer/src/main/java/hello/HelloBean.java
@@ -14,9 +14,7 @@ public class HelloBean implements Hello {
     private static final MyLogger logger = new MyLogger();
 
     public String sayHello(String caller) {
-        String logMsg = String.format("HelloBean.sayHello(%s)", caller);
-        System.out.println(logMsg);
-        logger.info(logMsg);
+        logger.info(String.format("Received invocation from %s", caller));
         return String.format("Hello %s", caller);
     }
 }
